601a935ee64fa8dcfda68cd0f3c62a1e416fd23f
Edwin Bryan Chalacan Araujo <ebchalacan@uce.edu.ec>
2020-08-03 23:14:18 -0500

    make it better

--- src/main/java/com/distribuida/db/ManejadorDb.java
+++ src/main/java/com/distribuida/db/ManejadorDb.java
@@ -18,40 +18,40 @@ public class ManejadorDb {
 	//es un metodo de tipo datasource el cual se inyectará cuando 
 	//lo necesitemos.
 	@Produces @ApplicationScoped
-	public DataSource db() {
-		
-		 BasicDataSource ds = new BasicDataSource();
+public DataSource db( ) {
+        
+        BasicDataSource ds = new BasicDataSource();
 
-		 
+ 
 
-	        URI dbUri;
-	        
-	        try {
-	            dbUri = new URI(System.getenv("DATABASE_URL"));
-	            String username = dbUri.getUserInfo().split(":")[0];
-	            String password = dbUri.getUserInfo().split(":")[1];
-	            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
-	            
-	            ds.setDriverClassName( "org.postgresql.Driver" );
-	            ds.setUrl( dbUrl );
-	            ds.setUsername( username );
-	            ds.setPassword( password );
-	        } 
-	        catch (URISyntaxException e) {
-	            e.printStackTrace();
-	            
-	            throw new RuntimeException( "no s epuede conectar a la base de datos" );
-	        }
+        URI dbUri;
+        
+        try {
+            dbUri = new URI(System.getenv("DATABASE_URL"));
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+            
+            ds.setDriverClassName( "org.postgresql.Driver" );
+            ds.setUrl( dbUrl );
+            ds.setUsername( username );
+            ds.setPassword( password );
+        } 
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+            
+            throw new RuntimeException( "no s epuede conectar a la base de datos" );
+        }
 
-	 
+ 
 
 
-//	        ds.setDriverClassName( "org.postgresql.Driver" );
-//	        ds.setUrl( "jdbc:postgresql://127.0.0.1:5432/distribuida" );
-//	        ds.setUsername( "postgres" );
-//	        ds.setPassword( "postgres" );
-	        
-	        return ds;
-	    }
+//        ds.setDriverClassName( "org.postgresql.Driver" );
+//        ds.setUrl( "jdbc:postgresql://127.0.0.1:5432/distribuida" );
+//        ds.setUsername( "postgres" );
+//        ds.setPassword( "postgres" );
+        
+        return ds;
+    }
 
 	}
