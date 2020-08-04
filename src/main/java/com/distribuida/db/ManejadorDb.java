54674bf5b12d79e50f0fbb11c60da37d7aad7051
Edwin Bryan Chalacan Araujo <ebchalacan@uce.edu.ec>
2020-08-03 22:55:55 -0500

    make it better

--- src/main/java/com/distribuida/db/ManejadorDb.java
+++ src/main/java/com/distribuida/db/ManejadorDb.java
@@ -22,24 +22,11 @@ public class ManejadorDb {
 		
 		BasicDataSource ds = new BasicDataSource();
 		
-URI dbUri;
-        
-        try {
-            dbUri = new URI(System.getenv("DATABASE_URL"));
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
-            
-            ds.setDriverClassName( "org.postgresql.Driver" );
-            ds.setUrl( dbUrl );
-            ds.setUsername( username );
-            ds.setPassword( password );
-        } 
-        catch (URISyntaxException e) {
-            e.printStackTrace();
-            
-            throw new RuntimeException( "no s epuede conectar a la base de datos" );
-        }
+		ds.setDriverClassName("org.postgresql.Driver");
+		ds.setUrl("jdbc:postgresql://ec2-52-202-66-191.compute-1.amazonaws.com:5432/d601dqtlqgse7j");
+		ds.setUsername("svprltmotmesyq");
+		ds.setPassword("e69d5bdc20722f8258dbf0edfd028b4f3adaf6eca32d7349357c1be935563fa4");
+		
 		
 		return ds ;
 	}
